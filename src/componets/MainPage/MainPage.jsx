58b72fc347617386daae6c8d30d4c2640fa61fba
Yevhen Chernenko <chernenko.eu@gmail.com>
2020-10-19 17:05:11 +0300

    simple jat styled css

--- src/componets/MainPage/MainPage.jsx
+++ src/componets/MainPage/MainPage.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import './MainPage.css';
 
 const MainPage = () => {
-    const [rate, setRate] = useState(null);
+    const [rate, setRate] = useState("...");
     const [convertFrom, setConvertFrom] = useState("USD");
     const [convertTo, setConvertTo] = useState("AUD");
     const [value, setValue] = useState(1);
@@ -38,6 +38,7 @@ const MainPage = () => {
     }
 
     const convert = () => {
+        setRate("...");
         axios.get(`https://api.openrates.io/latest?base=${convertFrom}&symbols=${convertTo}`).then(response => {
             const result = value * (response.data.rates[convertTo]);
             setRate(result.toFixed(2));
@@ -49,11 +50,12 @@ const MainPage = () => {
     return (
         <div className="column column-h column-v converter-wrapper">
             <div className="column converter-content">
-                <span>Jat App Currency Converter</span> 
+                <span className="app-title">Jat <span className="app-title-detail">App</span> Currency Converter</span> 
                 {/* Amount */}
-                <div className="row">
-                    <span>Amount</span>
+                <div className="row row-h convertor-row">
+                    <span className="convertor-text">Amount</span>
                     <input
+                        className="convertor-input"
                         name="value"
                         type="text"
                         value={value}
@@ -61,9 +63,10 @@ const MainPage = () => {
                     </input>
                 </div>
                 {/* From */}
-                <div className="row">
-                    <span>From Currency</span>
+                <div className="row row-h convertor-row">
+                    <span className="convertor-text">From</span>
                     <select
+                        className="convertor-input"
                         name="from"
                         onChange={(event) => selectValue(event)}
                         value={convertFrom}>
@@ -73,9 +76,10 @@ const MainPage = () => {
                     </select>
                 </div>
                 {/* To */}
-                <div className="row">
-                    <span>To Currency</span>
+                <div className="row row-h convertor-row">
+                    <span className="convertor-text">To</span>
                     <select
+                        className="convertor-input"
                         name="to"
                         onChange={(event) => selectValue(event)}
                         value={convertTo}>
@@ -85,7 +89,9 @@ const MainPage = () => {
                     </select>
                 </div>
                 {/* Result */}
-                <span>Result: {rate}</span>
+                <div className="row row-h">
+                    <span className="converter-result">{rate}</span>
+                </div>
             </div>
         </div>
     );

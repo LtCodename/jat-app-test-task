6981e71e8d01127eaabb42e67ce6d491de8ba0d6
Yevhen Chernenko <chernenko.eu@gmail.com>
2021-05-14 16:45:31 +0300

    Updates

---
+++ precache-manifest.c9bb8a4c2dca3f5448845ac0af6516f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "767932cc7b8743137251a85e2d809c72",
+    "url": "/jat-app-test-task/index.html"
+  },
+  {
+    "revision": "e62e306046699771014c",
+    "url": "/jat-app-test-task/static/css/main.f4a15efa.chunk.css"
+  },
+  {
+    "revision": "f6098ebeae63e4837de6",
+    "url": "/jat-app-test-task/static/js/2.95d53920.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/jat-app-test-task/static/js/2.95d53920.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e62e306046699771014c",
+    "url": "/jat-app-test-task/static/js/main.d6ca9e76.chunk.js"
+  },
+  {
+    "revision": "244c32b1cf7d5b5ac1f5",
+    "url": "/jat-app-test-task/static/js/runtime-main.da5a45eb.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/jat-app-test-task/precache-manifest.c9bb8a4c2dca3f5448845ac0af6516f6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jat-app-test-task/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
